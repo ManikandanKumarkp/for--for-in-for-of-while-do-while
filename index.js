3dacaa8115a20f99b09f893e3b9799875249808d
ManikandanKumarkp <manikandankumarkp@gmail.com>
2021-10-24 07:52:38 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-//for
+// //for
 // let i,
 // for(i=0; i<=10; i++){
 //   console.log(i)
+//   if(i==5){
+// break;
+//   }
 // }
 
 //  //for in
@@ -18,10 +21,18 @@ appDiv.innerHTML = `<h1>JS Starter</h1>`;
 // }
 
 
-// for of
-let num =[10,20,30,40]
-for(let m of num){
-  console.log(num)
-}
+// // for of
+// let num =[10,20,30,40]
+// for(let m of num){
+//   console.log(num)
+// }
+
+// let num ='mark'
+//  for(let m of num){
+//   console.log(m)
+//   if(m=='a'){
+//     break;
+//   }
+// }
 
 
