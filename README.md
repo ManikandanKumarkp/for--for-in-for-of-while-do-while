f78cd3b5d824d0685f324af5a504f2c3a5cf6b76
ManikandanKumarkp <manikandankumarkp@gmail.com>
2021-10-24 07:45:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-mqbipk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-mqbipk)
---
+++ index.js
@@ -0,0 +1,29 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+//for
+// let i,
+// for(i=0; i<=10; i++){
+//   console.log(i)
+// }
+
+//  //for in
+// let car={model:'unova', colour:'red', engine:'8hp'}
+// for(let x in car){
+//   console.log(car[x])
+// }
+
+
+// for of
+let num=[10,20,30,40]
+for(let no of num){
+if(num == 30){
+  break;
+}
+}
+
+
